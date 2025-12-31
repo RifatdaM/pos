28b49e7ed107cc48f09da3b54aeacbe8cb803648
immehedi49 <m.hasanrafin49@gmail.com>
2026-01-01 00:29:59 +0600

    done

--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -109,7 +109,7 @@ const Summary = ({ cartTotal = 0 }) => {
 
             {/* Discount & Tax Row */}
             <div className="">
-              <div className="flex items-center w-full gap-4">
+              <div className="flex items-center w-full gap-1">
                 <label className="text-gray-500 whitespace-nowrap">
                   Discount
                 </label>
